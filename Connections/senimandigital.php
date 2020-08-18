8ba079372963c8bbba9597a2409cea20e11a5aec
Eboy <eboy_karya_amatir@yahoo.co.id>
2020-08-18 00:54:49 +0000

    Script minimal untuk koneksi MySQLi Procedural

---
+++ Connections/senimandigital.php
@@ -0,0 +1,10 @@
+<?php
+# FileName="Connection_php_mysqli_procedural.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_senimandigital = "localhost";
+$database_senimandigital = "dreamweaver_phpmysql";
+$username_senimandigital = "senimandigital";
+$password_senimandigital = "senimandigital";
+$senimandigital = mysqli_connect($hostname_senimandigital, $username_senimandigital, $password_senimandigital) or mysqli_connect_error()); 
+?>
